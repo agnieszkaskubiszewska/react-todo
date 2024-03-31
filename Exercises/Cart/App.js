cd8b4bac49d29fab436935bbcae7a1fdf00db19b
Agnieszka Skubiszewska <agnieszkaskubiszewska@MacBook-Pro-Agnieszka.local>
2024-03-31 15:46:15 +0200

    styles

--- Exercises/Cart/App.js
+++ Exercises/Cart/App.js
@@ -14,13 +14,19 @@ class App extends React.Component {
             shopingCart:this.state.shopingCart +1
         })
     }
-    
+    handleBuy =() =>{
+        this.setState({
+            availableProducts:this.state.availableProducts - this.state.shopingCart,
+            shopingCart:0
+        })
+    }
     render() {
         return(
             <div>
             <button onClick={this.handleRemoveFromCart} disabled={this.state.shopingCart ? false : true}>-</button>
-            <span> {this.state.shopingCart} </span>
+            <span style={this.state.shopingCart === 0 ? {opacity:0.3}: {}}> {this.state.shopingCart} </span>
             <button disabled={this.state.shopingCart ===this.state.availableProducts? true : false }   onClick={this.handleAddToCard}>+</button>
+            {this.state.shopingCart > 0 && <button onClick={this.handleBuy}>KUP</button>}
             </div>
         )
     }
